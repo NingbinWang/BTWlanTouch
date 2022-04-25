3f724acfcd2a3386422025288fcb96dedf7cbd69
kingalex <188101696@qq.com>
2022-04-26 00:15:24 +0800

    first commit

---
+++ 01.SoftWare/BTWlanTouch/BTWlanTouch/BTWlanTouchApp.swift
@@ -0,0 +1,17 @@
+//
+//  BTWlanTouchApp.swift
+//  BTWlanTouch
+//
+//  Created by 王宁斌 on 2022/4/26.
+//
+
+import SwiftUI
+
+@main
+struct BTWlanTouchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
